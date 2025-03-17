252f2146052f80b822831531fdb4b3d57b85d29b
ruann3res <ruanpablo1218@gmail.com>
2025-03-17 19:55:58 -0300

    feat: implement theme context and UI components; add Button and Input components with styling

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { ThemeProvider } from "@/contexts";
+
+function App() {
+
+  return (
+    <ThemeProvider>
+      <h1>UAI PY LP</h1>
+    </ThemeProvider>
+
+  )
+}
+
+export default App
---
+++ src/contexts/theme-context.tsx
@@ -0,0 +1,35 @@
+import { createContext, useEffect, useState } from 'react'
+
+type Theme = 'light' | 'dark'
+
+type ThemeContextType = {
+  theme: Theme
+  toggleTheme: () => void
+}
+
+export const ThemeContext = createContext<ThemeContextType | undefined>(undefined)
+
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+  const [theme, setTheme] = useState<Theme>(() => {
+    const savedTheme = localStorage.getItem('theme')
+    return (savedTheme as Theme) || 'light'
+  })
+
+  useEffect(() => {
+    const root = window.document.documentElement
+    root.classList.remove('light', 'dark')
+    root.classList.add(theme)
+    localStorage.setItem('theme', theme)
+  }, [theme])
+
+  const toggleTheme = () => {
+    setTheme(prev => prev === 'light' ? 'dark' : 'light')
+  }
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
+
