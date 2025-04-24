40b6dc3e70c5a5e04a3bc270e4b30257d208f879
Ana Clara <anaccustodio8@gmail.com>
2025-04-24 11:06:20 -0300

    feat: footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { ThemeProvider } from "@/contexts";
 import { Features } from "./components/sections/Features";
 import { Hero } from "./components/sections/Hero";
+import { Footer } from "./components/sections/Footer";
 
 function App() {
     return (
         <ThemeProvider>
           <Hero />
           {/* <Features /> */}
+          <Footer />
         </ThemeProvider>
     );
 }
