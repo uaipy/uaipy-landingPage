893aba483389caf3ea158726d347380fc06e0f17
Ana Clara <anaccustodio8@gmail.com>
2025-05-26 10:36:08 -0300

    feat: uai py team section

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Features } from "./components/sections/Features";
 import { Hero } from "./components/sections/Hero";
 import { Footer } from "./components/sections/Footer";
 import { ProjectConcept } from "./components/sections/ProjectConcept";
+import { Team } from "./components/sections/Team";
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
           <Hero />
           <ProjectConcept />
           <Features />
+          <Team />
           <Footer />
         </ThemeProvider>
     );
