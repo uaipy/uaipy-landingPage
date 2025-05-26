00c83d8b57deb64980233860da2df2f92e2cda94
Ana Clara <anaccustodio8@gmail.com>
2025-05-26 14:44:44 -0300

    chore: fixed css and added new size variation to button component

--- src/components/sections/Team/index.tsx
+++ src/components/sections/Team/index.tsx
@@ -2,8 +2,8 @@ import ContentBlock from "@/components/ui/ContentBlock"
 
 export const Team = () => {
     return (
-        <section className="px-10 my-16 md:px-15 md:my-10 lg:px-20 xl:px-35">
-            <ContentBlock marginDirection="md:ml-10 xl:ml-15" title="Nosso time" subheading="Colaboradores envolvidos" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In placerat neque ut risus iaculis tristique. Fusce leo mi, elementum ut elit in, lobortis consectetur tortor.Lorem ipsum dolor sit amet, consectetur adipiscing elit. In placerat neque ut risus iaculis tristique. Fusce leo mi, elementum ut elit in, lobortis consectetur tortor." flexDirection="flex-col justify-center md:flex-row" showBtn={false} imgClassName="mb-12"/>
+        <section className="px-10 md:px-15 lg:px-20 xl:px-35">
+            <ContentBlock marginDirection="md:ml-10 xl:ml-15" title="Nosso time" subheading="Colaboradores envolvidos" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In placerat neque ut risus iaculis tristique. Fusce leo mi, elementum ut elit in, lobortis consectetur tortor.Lorem ipsum dolor sit amet, consectetur adipiscing elit. In placerat neque ut risus iaculis tristique. Fusce leo mi, elementum ut elit in, lobortis consectetur tortor." flexDirection="flex-col justify-center md:flex-row" showBtn={false} imgClassName="mb-12 md:mb-0"/>
         </section>
     )
 }
