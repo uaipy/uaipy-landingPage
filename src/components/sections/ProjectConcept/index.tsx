ff2f191b306c16a18ce765b7e39fd760707cf4b7
Ana Clara <anaccustodio8@gmail.com>
2025-07-11 10:38:09 -0300

    feat: adding images to first carousel and updating first ProjectConcept section

--- src/components/sections/ProjectConcept/index.tsx
+++ src/components/sections/ProjectConcept/index.tsx
@@ -1,5 +1,8 @@
 import ContentBlock from "@/components/ui/ContentBlock";
 import Metrics from "@/components/ui/Metrics"
+import image1 from "@/assets/inicio_1.jpeg";
+import image2 from "@/assets/inicio_2.jpeg";
+import image3 from "@/assets/inicio_3.jpeg";
 
 const metrics = [
     {
@@ -33,7 +36,7 @@ export const ProjectConcept = () => {
                 ))}
             </div>
             <div className="hidden md:block">
-                <ContentBlock marginDirection="md:ml-10 xl:ml-15" title="O início de tudo" subheading="Conceito do Projeto" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In placerat neque ut risus iaculis tristique. Fusce leo mi, elementum ut elit in, lobortis consectetur tortor.Lorem ipsum dolor sit amet, consectetur adipiscing elit. In placerat neque ut risus iaculis tristique. Fusce leo mi, elementum ut elit in, lobortis consectetur tortor." showBtn={true} btnText="Conheça+" btnVariant="gradient" btnClassName="mt-4 cursor-pointer" imgClassName="hidden md:block"/>
+                <ContentBlock marginDirection="md:ml-10 lg:ml-7 xl:ml-15" title="O início de tudo" subheading="Conceito do Projeto" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In placerat neque ut risus iaculis tristique. Fusce leo mi, elementum ut elit in, lobortis consectetur tortor.Lorem ipsum dolor sit amet, consectetur adipiscing elit. In placerat neque ut risus iaculis tristique. Fusce leo mi, elementum ut elit in, lobortis consectetur tortor." showBtn={true} btnText="Conheça+" btnVariant="gradient" btnClassName="mt-4 cursor-pointer" imgClassName="hidden aspect-square object-cover md:block md:rounded-full" carouselImages={[image1, image2, image3]}/>
             </div>
             <div className="mt-16">
                 <ContentBlock marginDirection="md:mr-10 xl:mr-15" title="Entenda melhor" subheading="Conceito do Projeto" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In placerat neque ut risus iaculis tristique. Fusce leo mi, elementum ut elit in, lobortis consectetur tortor.Lorem ipsum dolor sit amet, consectetur adipiscing elit. In placerat neque ut risus iaculis tristique. Fusce leo mi, elementum ut elit in, lobortis consectetur tortor." flexDirection="flex-col justify-center md:flex-row-reverse" showBtn={true} btnText="Saiba mais →" btnVariant="outline" btnClassName="mt-4 cursor-pointer" imgClassName="mb-12 md:mb-0"/>
