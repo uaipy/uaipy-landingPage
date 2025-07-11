33dd1583c1fffbde16afe6c12a5ca09ecc60b9d3
Ana Clara <anaccustodio8@gmail.com>
2025-07-11 14:11:26 -0300

    feat: adding new photos for carousel and updating TechEvents section

--- src/components/sections/TechEvents/index.tsx
+++ src/components/sections/TechEvents/index.tsx
@@ -1,9 +1,12 @@
 import ContentBlock from "@/components/ui/ContentBlock";
+import eventos1 from "@/assets/eventos_1.jpg";
+import eventos2 from "@/assets/eventos_2.jpeg";
+import eventos3 from "@/assets/eventos_3.jpeg";
 
 export const TechEvents = () => {
     return (
         <section className="px-10 md:px-15 lg:px-20 xl:px-35">
-            <ContentBlock marginDirection="md:mr-15 xl:mr-20" title="Eventos" subheading="Participações em eventos" description="Acho legal trazer que já estamos marcando presença em eventos tech da região e afins. Fusce leo mi, elementum ut elit in." flexDirection="flex-col md:flex-row-reverse" imgClassName="mb-12 rounded-none w-full h-auto md:w-100 lg:w-120 xl:w-150" />
+            <ContentBlock marginDirection="md:mr-15 xl:mr-20" title="Eventos" subheading="Participações em eventos" description="Nosso projeto já marcou presença em diversos eventos de tecnologia, levando inovação e impacto social por onde passa. A cada participação, fortalecemos conexões, trocamos experiências e mostramos como a tecnologia pode transformar realidades." flexDirection="flex-col md:flex-row-reverse" carouselImages={[eventos1, eventos2, eventos3]} imgClassName="mb-12 aspect-3/2 object-cover" />
         </section>
     );
 }
