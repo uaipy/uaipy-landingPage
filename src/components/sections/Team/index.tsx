91d9dc6ff527bf42709f1514dc2aefa8b7afff2b
Ana Clara <anaccustodio8@gmail.com>
2025-07-18 20:02:11 -0300

    feat: replacing placeholder text

--- src/components/sections/Team/index.tsx
+++ src/components/sections/Team/index.tsx
@@ -4,7 +4,7 @@ import team from "@/assets/time.jpg"
 export const Team = () => {
     return (
         <section className="px-10 md:px-15 lg:px-20 xl:px-35">
-            <ContentBlock marginDirection="md:ml-10 xl:ml-15" title="Nosso time" subheading="Colaboradores envolvidos" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In placerat neque ut risus iaculis tristique. Fusce leo mi, elementum ut elit in, lobortis consectetur tortor.Lorem ipsum dolor sit amet, consectetur adipiscing elit. In placerat neque ut risus iaculis tristique. Fusce leo mi, elementum ut elit in, lobortis consectetur tortor." flexDirection="flex-col justify-center md:flex-row" showBtn={false} staticImage={team} imgClassName="max-w-full md:max-w-6/10 lg:max-w-5/10 mb-12 md:mb-0"/>
+            <ContentBlock marginDirection="md:ml-10 xl:ml-15" title="Nosso time" subheading="Colaboradores envolvidos" description="Nosso time é composto por estudantes de cursos das áreas de engenharia, tecnologia, ciências agrárias e ciências econômicas. A interdisciplinariedade presente no grupo é fundamental para a expansão e desenvolvimento do projeto, uma vez que o conhecimento é plural." flexDirection="flex-col justify-center md:flex-row" showBtn={false} staticImage={team} imgClassName="max-w-full md:max-w-6/10 lg:max-w-5/10 mb-12 md:mb-0"/>
         </section>
     )
 }
